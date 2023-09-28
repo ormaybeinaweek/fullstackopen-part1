3df651ebd4c1422c761b617aa8f003b4d0694a1a
Alperen <67557734+ormaybeinaweek@users.noreply.github.com>
2023-09-28 22:30:17 +0300

    Update App.jsx

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,26 +1,26 @@
 import { useState } from 'react'
 
-const Header = props => {
-  return <h1>{props.course}</h1>;
-};
+const Header = props => (
+  <h1>{props.course}</h1>
+)
 
-const Part = props => {
-   return <p>{props.part} {props.exercises}</p>
-}
+const Part = props => (
+   <p>{props.part} {props.exercises}</p>
+)
 
-const Content = props => {
-  return <div>
+const Content = props => (
+  <div>
     <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
     <Part part={props.parts[1].name} exercises={props.parts[1].exercises}/>
     <Part part={props.parts[2].name} exercises={props.parts[2].exercises}/>
   </div>
-}
+)
 
-const Total = (props) => (
+const Total = props => (
   <div>
     <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
   </div>
-);
+)
 
 function App() {
   const course = {
